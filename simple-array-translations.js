81c47162d7c000750aefb4c30ad7adabcfb89814
Eric Lifka <eric.lifka@gmail.com>
2015-10-02 22:57:42 -0400

    add super naive implementations for variables and additions, fails the complex test case spectacularly

--- simple-array-translations.js
+++ simple-array-translations.js
@@ -1,3 +1,20 @@
+function letToVar(arr) {
+  return `var ${arr[1]} = ${arr[2]};`;
+}
+
+function addition(arr) {
+  return `${arr[1]} + ${arr[2]};`;
+}
+
 export const toJsString = function (arr) {
+  if (typeof arr !== 'object' || typeof arr.length !== 'number') {
+    return "";
+  }
+
+  switch(arr[0]) {
+    case 'let': return letToVar(arr);
+    case '+': return addition(arr);
+  }
+
   return "";
 };
