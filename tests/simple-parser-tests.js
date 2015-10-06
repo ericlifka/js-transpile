e38f06521cb695186ae2858342b0346e6e9478ed
Eric Lifka <eric.lifka@gmail.com>
2015-10-05 23:09:23 -0400

    add some failing tests to spec out parser behavior

--- tests/simple-parser-tests.js
+++ tests/simple-parser-tests.js
@@ -5,5 +5,20 @@ describe('simple-parser', () => {
   describe('#parse()', () => {
     it('should return an array that represents the parsed list structure', () =>
       parse().should.be.an.Array());
+
+    it('should parse an empty list', () =>
+      parse('()').should.equal([]));
+
+    it('should parse a list of symbols', () =>
+      parse('(a b c)').should.equal(['a', 'b', 'c']));
+
+    it('should parse a nested list', () =>
+      parse('(a (b c) d)').should.equal(['a', ['b', 'c'], 'd']));
+
+    it('should handle strings', () =>
+      parse('("abc")').should.equal(['"abc"']));
+
+    it('should handle whitespace in strings', () =>
+      parse('("abc 123")').should.equal(['"abc 123"']));
   });
 });
