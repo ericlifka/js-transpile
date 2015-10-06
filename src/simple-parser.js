96af8b7ee5f031691abdbb361e799f614765f33b
Eric Lifka <eric.lifka@gmail.com>
2015-10-05 23:42:54 -0400

    layout some parsing structure

--- src/simple-parser.js
+++ src/simple-parser.js
@@ -1,4 +1,43 @@
+import { isWhitespaceChar } from './utils';
+
+function _parse_list(state) {
+  state.position++; // (
+  const list = [];
+  while (state.input[state.position] !== ')') {
+    list.push(_parse(state));
+  }
+  state.position++; // )
+  return list;
+}
+
+function _parse_string(state) {
+
+}
+
+function _parse_symbol(state) {
+
+}
+
+function _parse(state) {
+  while (isWhitespaceChar(state.input[state.position])) {
+    state.position++;
+  }
+  const ch = state.input[state.position];
+
+  if (ch === '(') {
+    return _parse_list(state);
+  }
+
+  if (ch === '"' || ch === "'") {
+    return _parse_string(state);
+  }
+
+  return _parse_symbol(state);
+}
 
 export function parse(input) {
-  return [];
+  return _parse({
+    input,
+    position: 0
+  });
 }
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
-export const isArray = (arr) => typeof arr === 'object' && typeof arr.length === 'number';
+export const isArray = arr =>
+  typeof arr === 'object' &&
+  typeof arr.length === 'number';
+
+export const isWhitespaceChar = char =>
+  /\s/.test(char);
+
