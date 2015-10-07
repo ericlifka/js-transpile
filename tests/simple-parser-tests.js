6e1124e56ee70163bc6160fa8e5dfa29f6543539
Eric Lifka <eric.lifka@gmail.com>
2015-10-06 21:40:10 -0400

    needs to be deep equal to check that the arrays are similar

--- tests/simple-parser-tests.js
+++ tests/simple-parser-tests.js
@@ -4,21 +4,21 @@ import { parse } from '../src/simple-parser';
 describe('simple-parser', () => {
   describe('#parse()', () => {
     it('should return an array that represents the parsed list structure', () =>
-      parse().should.be.an.Array());
+      parse('()').should.be.an.Array());
 
     it('should parse an empty list', () =>
-      parse('()').should.equal([]));
+      parse('()').should.deepEqual([]));
 
     it('should parse a list of symbols', () =>
-      parse('(a b c)').should.equal(['a', 'b', 'c']));
+      parse('(a b c)').should.deepEqual(['a', 'b', 'c']));
 
     it('should parse a nested list', () =>
-      parse('(a (b c) d)').should.equal(['a', ['b', 'c'], 'd']));
+      parse('(a (b c) d)').should.deepEqual(['a', ['b', 'c'], 'd']));
 
     it('should handle strings', () =>
-      parse('("abc")').should.equal(['"abc"']));
+      parse('("abc")').should.deepEqual(['"abc"']));
 
     it('should handle whitespace in strings', () =>
-      parse('("abc 123")').should.equal(['"abc 123"']));
+      parse('("abc 123")').should.deepEqual(['"abc 123"']));
   });
 });
