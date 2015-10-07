944f4e8e7b31b01798e38246e2d753dfbe622633
Eric Lifka <eric.lifka@gmail.com>
2015-10-06 23:36:01 -0400

    add some transform tests, including a math one that breaks the parser

--- tests/transform-tests.js
+++ tests/transform-tests.js
@@ -4,4 +4,15 @@ import { transformString } from '../src/transform';
 describe('transform', () => {
   it('should turn a string into a string', () =>
     transformString("()").should.be.a.String());
+
+  describe('variables', () => {
+    it('(let a 2)', () =>
+      transformString("(let a 2)").should.equal("var a = 2;"));
+  });
+
+  describe('math', () => {
+    it('(+ 1 2)', () =>
+      transformString('(+ 1 2)').should.equal("1 + 2"));
+  });
+
 });
