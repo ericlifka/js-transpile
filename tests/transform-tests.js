1dadfca3c08e966310fb1163251c5aeb80c79e5d
Eric Lifka <eric.lifka@gmail.com>
2015-11-25 17:42:17 -0500

    math tests were easy fixes, the tests shouldn't expect so many parenthesis now because the statements know when they're actually needed

--- tests/transform-tests.js
+++ tests/transform-tests.js
@@ -16,16 +16,16 @@ describe('transform', () => {
   });
 
   describe('math', () => {
-    test('(+ 1 2)', '(1 + 2)');
-    test('(- 9 6)', '(9 - 6)');
-    test('(* 2 3)', '(2 * 3)');
-    test('(/ 4 2)', '(4 / 2)');
-    test('(* (+ 2 4) (- 5 3))', '((2 + 4) * (5 - 3))');
-    test('(+ 2 3 4 5)', '(2 + 3 + 4 + 5)');
+    test('(+ 1 2)', '1 + 2');
+    test('(- 9 6)', '9 - 6');
+    test('(* 2 3)', '2 * 3');
+    test('(/ 4 2)', '4 / 2');
+    test('(* (+ 2 4) (- 5 3))', '(2 + 4) * (5 - 3)');
+    test('(+ 2 3 4 5)', '2 + 3 + 4 + 5');
 
-    test('(Math.pow 2 4)', 'Math.pow( 2, 4 )');
-    test('(Math.abs -4)', 'Math.abs( -4 )');
-    test('(Math.pow (* 2 3) (- 2 4))', 'Math.pow( (2 * 3), (2 - 4) )');
+    test('(Math.pow 2 4)', 'Math.pow(2, 4)');
+    test('(Math.abs -4)', 'Math.abs(-4)');
+    test('(Math.pow (* 2 3) (- 2 4))', 'Math.pow((2 * 3), (2 - 4))');
   });
 
   describe('control flow', () => {
