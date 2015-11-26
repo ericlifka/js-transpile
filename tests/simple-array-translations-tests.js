1aba58a88c1fa9bccb11674fc46c53ec20df24b0
Eric Lifka <eric.lifka@gmail.com>
2015-11-26 01:04:21 -0500

    those tests just needed to print the statements at the end

--- tests/simple-array-translations-tests.js
+++ tests/simple-array-translations-tests.js
@@ -3,8 +3,8 @@ import { toJsTree } from '../src/simple-array-translations';
 
 describe('simple-array-translations', () => {
   describe('#toJsTree()', () => {
-    it('should handle a simple variable assignment', () => toJsTree([ 'let', 'a', '5' ]).should.equal("var a = 5;"));
-    it('should handle a simple math statement', () => toJsTree([ '+', '2', '3' ]).should.equal("(2 + 3)"));
-    it('should handle a math statement into an assignment', () => toJsTree([ 'let', 'a', [ '+', '2', '3' ] ]).should.equal("var a = (2 + 3);"));
+    it('should handle a simple variable assignment', () => toJsTree([ 'let', 'a', '5' ]).printString().should.equal("var a = 5;"));
+    it('should handle a simple math statement', () => toJsTree([ '+', '2', '3' ]).printString().should.equal("2 + 3"));
+    it('should handle a math statement into an assignment', () => toJsTree([ 'let', 'a', [ '+', '2', '3' ] ]).printString().should.equal("var a = (2 + 3);"));
   });
 });
