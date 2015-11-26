e16adc05f0b40e3914f21c9b8d6de70e13a33a2a
Eric Lifka <eric.lifka@gmail.com>
2015-11-26 00:58:21 -0500

    fix up those two tests by removing the last of the extraneous parenthesis

--- tests/transform-tests.js
+++ tests/transform-tests.js
@@ -37,9 +37,9 @@ describe('transform', () => {
   describe.only('function literals', () => {
     test('(function ())', 'function () {return null;}');
     test('(function () 4)', 'function () {return 4;}');
-    test('(function (x y) (+ x y))', 'function (x, y) {return (x + y);}');
-    test('(function adder (x y) (+ x y))', 'function adder(x, y) {return (x + y);}');
-    test('(function (x) (function (y) (+ x y)))', 'function (x) {return function (y) { return (x + y);};}');
+    test('(function (x y) (+ x y))', 'function (x, y) {return x + y;}');
+    test('(function adder (x y) (+ x y))', 'function adder(x, y) {return x + y;}');
+    test('(function (x) (function (y) (+ x y)))', 'function (x) {return function (y) { return x + y;};}');
   });
 
   describe('function calls', () => {
