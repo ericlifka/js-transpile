a446b4582ae42a11dbc626e301557709c0f6e31d
Eric Lifka <eric.lifka@gmail.com>
2015-11-29 22:51:16 -0500

    use a switch to catch simple literal translations

--- src/builtin-macros.js
+++ src/builtin-macros.js
@@ -1,4 +1,10 @@
-
 export default function (arr, options) {
+  switch (arr) {
+    case 'nil': return 'null';
+    case 't': return 'true';
+    case 'f': return 'false';
+    default: break;
+  }
+
   return arr;
 }
